50dd856a9fe1ff8dde599bd97b54b6fd0591a715
greem <83728244+greemjvhgv@users.noreply.github.com>
2025-03-27 00:18:32 +0200

    Made Magic 8 Ball

---
+++ Magic 8 Ball/Magic 8 Ball/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magic_8_Ball
+{
+    public class AnswerGenerator
+    {
+        private static string[] answers = { "Yes Definitely" , "Signs Point To Yes" , "Most Likely" , "Without A Doubt" ,
+                "Reply Hazy, Try Again" , "Ask again later" , "Cannot predict now" , "Better Not Tell You Now",
+                "My Reply Is No" , "My Sources Say No" , "Outlook Not So Good" , "Very Doubtful" };
+
+        private static string[] thinkingMessages = { 
+            "The Magic 8 Ball is pondering your question...","Shaking the Magic 8 Ball... Hold on.",
+            "Consulting the spirits... Please wait.", "The future is unclear... Let me check again.",
+            "The Magic 8 Ball is deep in thought...", "Rolling the mystical dice... Almost ready.",
+            "Hmmm... This one is tricky.", "I must ask the cosmic forces... Give me a moment..." };
+
+        public static string GenerateAnswer()
+        {
+            var rand = new Random();
+            string randomAnswer = answers[rand.Next(answers.Length)];
+            return $"{randomAnswer}";
+        }
+
+        public static string ThinkingMessageGenerate()
+        {
+            var rand = new Random();
+            string randomThinkingMessage = thinkingMessages[rand.Next(thinkingMessages.Length)];
+            return $"{randomThinkingMessage}";
+        }
+    }
+}
---
+++ Magic 8 Ball/Magic 8 Ball/Program.cs
@@ -0,0 +1,44 @@
+﻿using Magic_8_Ball;
+
+var randoAnswer = AnswerGenerator.GenerateAnswer();
+var randomThinkingMessage = AnswerGenerator.ThinkingMessageGenerate();
+
+
+Console.ForegroundColor = ConsoleColor.Blue;
+    string text = @"
+
+     _______               __            ______      ______         __ __ 
+    |   |   |.---.-.-----.|__|.----.    |  __  |    |   __ \.---.-.|  |  |
+    |       ||  _  |  _  ||  ||  __|    |  __  |    |   __ <|  _  ||  |  |
+    |__|_|__||___._|___  ||__||____|    |______|    |______/|___._||__|__|
+                   |_____|                                                
+                                                              
+    ";
+        Console.WriteLine(text);
+
+    Console.ForegroundColor = ConsoleColor.White;
+
+    Console.WriteLine("-Ask The Magic 8 Ball A Question!- ");
+       var question = Console.ReadLine();
+
+    while (string.IsNullOrEmpty(question))
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("-!Please Enter A Question!-");
+        Console.WriteLine(" ");
+        Console.ForegroundColor = ConsoleColor.White;
+            question = Console.ReadLine();
+     }
+
+
+    Console.WriteLine($"\n{randomThinkingMessage}");
+        Thread.Sleep(2000);
+
+  Console.ForegroundColor = ConsoleColor.Blue; 
+
+    Console.WriteLine($"{randoAnswer}");
+
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.ReadKey();
+
